dc6b26c63fd50f6a61e454c4ece8ad6feb2aaaff
Nghiem Gia Bao <bao15022016@gmail.com>
2022-08-23 16:03:10 +0700

    Finish Error message catching

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+// Declaring Array to store the elements
 const inputArray = [];
 const errorMessageArray = [];
 
@@ -19,13 +20,17 @@ parentNodeNumber.appendChild(errorMessageNumber);
 
 const expireMonth = document.getElementById('expire_month');
 inputArray.push(expireMonth);
+const errorMessageExpireMonth = document.createElement("p");
+errorMessageArray.push(errorMessageExpireMonth);
+const parentNodeExpireMonth = expireMonth.parentNode;
+parentNodeExpireMonth.appendChild(errorMessageExpireMonth);
+
 const expireYear = document.getElementById('expire_year');
 inputArray.push(expireYear);
-const errorMessageExpire = document.createElement("p");
-errorMessageArray.push(errorMessageExpire);
-errorMessageArray.push(errorMessageExpire);
-const parentNodeExpire = expireYear.parentNode;
-parentNodeExpire.appendChild(errorMessageExpire);
+const errorMessageExpireYear = document.createElement("p");
+errorMessageArray.push(errorMessageExpireYear);
+const parentNodeExpireYear = expireYear.parentNode;
+parentNodeExpireYear.appendChild(errorMessageExpireYear);
 
 
 const cvc = document.getElementById('cvc');
@@ -35,44 +40,114 @@ errorMessageArray.push(errorMessageCvc);
 const parentNodeCvc = cvc.parentNode;
 parentNodeCvc.appendChild(errorMessageCvc);
 
+//Function Checking 
+function checkName(name) {
+    const regexName = /[a-zA-z ]{1,21}/;
+    const matchVar = name.match(regexName);
+    // console.log(matchVar);
+    if(matchVar[0] === name) {
+        return true;
+    }
+    return false;
+}
+
+function checkNumber(number) {
+    const regexNumber = /[0-9]{15,16}/;
+    const matchVar = number.match(regexNumber);
+    // console.log(matchVar);
+    if(matchVar !== null) {
+        if(matchVar[0] == number) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkMonth(month) {
+    console.log(Math.floor(month));
+    if(Math.floor(month) > 12 || Math.floor(month) < 1) {
+        return false;
+    }
+    return true;
+}
+
+function checkYear(year) {
+    const regexYear = /\d{2}/;
+    const matchVar = year.match(regexYear);
+    // console.log(matchVar);
+    if(matchVar !== null) {
+        if(matchVar[0] === year) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkCvc(cvc) {
+    const regexCvc = /\d{3,4}/;
+    const matchVar = cvc.match(regexCvc);
+    // console.log(matchVar);
+    if(matchVar !== null) {
+        if(matchVar[0] === cvc) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Validating data
 function validateData(){
     function createErrorMessage(inputElement, errorMessage){
         const loopAction = () => {
             inputElement.style.border = "2px solid red";
             errorMessage.style.marginBottom = "16px";
         }
+        const changeBack = () => {
+            inputElement.style.border = "1px solid black";
+            errorMessage.innerHTML = '';
+            errorMessage.style.marginBottom = "0px";
+        }
         if(inputElement.value === ""){
             loopAction();
             errorMessage.innerHTML = "*Information required*";
         }else if(inputElement === name){
-            if(inputElement.value !== /[a-zA-z ]{1,21}/){
+            if(checkName(inputElement.value) === false){
                 loopAction();
-                errorMessage.innerHTML = "*Name must contain only maximum 21 words*";
+                errorMessage.innerHTML = "*Invalid Name*";
+            }else{
+                changeBack();
             }
         }else if(inputElement === number){
-            if(inputElement.value !== /[\d]{15,16}/){
+            if(checkNumber(inputElement.value) === false){
                 loopAction();
-                errorMessage.innerHTML = "*Number must contain only 15 to 16 numbers*";
-            }   
+                errorMessage.innerHTML = "*Invalid Number*";
+            }else{
+                changeBack();
+            }
         }else if(inputElement === expireMonth){
-            if(inputElement.value > 12 || inputElement < 1){
+            if(checkMonth(inputElement.value) === false){
                 loopAction();
-                errorMessage.innerHTML = "*Invalid months*";
+                errorMessage.innerHTML = "*Invalid Months*";
+            }else{
+                changeBack();
             }
         }else if(inputElement === expireYear){
-            if(inputElement.value !== /2\d{3}/){
+            if(checkYear(inputElement.value) === false){
                 loopAction();
-                errorMessage.innerHTML = "*Invalid years*";
+                errorMessage.innerHTML = "*Invalid Year*";
+            }else{
+                changeBack();
             }
         }else if(inputElement === cvc){
-            if(inputElement.value !== /\d{3,4}/){
+            if(checkCvc(inputElement.value) === false){
                 loopAction();
-                errorMessage.innerMessage = "*Invalid cvc*";
+                errorMessage.innerHTML = "*Invalid CVC*";
+            }else{
+                changeBack();
             }
-        }else{
-            inputElement.style.border = "1px solid black";
-            errorMessage.innerHTML = '';
-            errorMessage.style.marginBottom = "0px";
+        }
+        else{
+            changeBack();
         }
     }
     
